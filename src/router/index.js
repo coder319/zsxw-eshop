a8b821ca1a2fad160c9c16d6c9a8a4725a377088
zc806406149 <806406149@qq.com>
2020-12-02 22:58:58 +0800

    lalala

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 // 懒加载页面组件
 const Home = () => import('../views/Home/Home.vue')
-
+const Cart = () => import('../views/Cart/Cart.vue')
 Vue.use(VueRouter)
 
 const routes = [
@@ -14,6 +14,11 @@ const routes = [
     path: '/home',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: Cart
   }
 ]
 
--- src/views/Cart/Cart.vue
+++ src/views/Cart/Cart.vue
@@ -1,15 +1,178 @@
 <template>
+    <a-table :columns="columns" :data-source="data">
+        <a slot="name" slot-scope="text">{{ text }}</a>
+        <span slot="customTitle"><a-icon type="smile-o" /> Name</span>
+        <span slot="tags" slot-scope="tags">
+      <a-tag
+              v-for="tag in tags"
+              :key="tag"
+              :color="tag === 'loser' ? 'volcano' : tag.length > 5 ? 'geekblue' : 'green'"
+      >
+        {{ tag.toUpperCase() }}
+      </a-tag>
+    </span>
+        <span slot="action" slot-scope="text, record">
+      <a>Invite 一 {{ record.name }}</a>
+      <a-divider type="vertical" />
+      <a>Delete</a>
+      <a-divider type="vertical" />
+      <a class="ant-dropdown-link"> More actions <a-icon type="down" /> </a>
+    </span>
+    </a-table>
 </template>
 
+
 <script>
-export default {
+    const columns = [
+        {
+            title: '名称',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: '单价/元',
+            dataIndex: 'Price',
+            key: 'Price',
+        },
+        {
+            title: '数量',
+            dataIndex: 'number',
+            key: 'number',
+        },
+        {
+            title: '小计',
+            key: 'Prices',
+            dataIndex: 'Prices',
+        },
+        {
+            title: '操作',
+            key: 'action',
+        },
+    ];
+
+    const data = [
+        {
+            key: '1',
+            name: 'John Brown',
+            Price: 32,
+            number: '1',
+            Prices:Sum(),    /*这个我该这么加*/
+        }
+    ];
+
+    export default {
   name: "Cart",
-  data () {
-    return {
-    };
-  }
+    data() {
+        return {
+            data,
+            columns,
+        };
+    },
+    methods:{
+        Numberdown(n){
+            if(n==1)
+            {
+                return ;
+            }
+            n--;/*这样改数目得不得变哦*/
+        },
+        Numberup(n){
+            n++; /*这样改数目得不得变哦*/
+        },
+        Sum(price,n){
+            return price * n;
+        }
+    }
 }
 </script>
 
 <style>
 </style>
+<!--原代码
+<template>
+  <a-table :columns="columns" :data-source="data">
+    <a slot="name" slot-scope="text">{{ text }}</a>
+    <span slot="customTitle"><a-icon type="smile-o" /> Name</span>
+    <span slot="tags" slot-scope="tags">
+      <a-tag
+        v-for="tag in tags"
+        :key="tag"
+        :color="tag === 'loser' ? 'volcano' : tag.length > 5 ? 'geekblue' : 'green'"
+      >
+        {{ tag.toUpperCase() }}
+      </a-tag>
+    </span>
+    <span slot="action" slot-scope="text, record">
+      <a>Invite 一 {{ record.name }}</a>
+      <a-divider type="vertical" />
+      <a>Delete</a>
+      <a-divider type="vertical" />
+      <a class="ant-dropdown-link"> More actions <a-icon type="down" /> </a>
+    </span>
+  </a-table>
+</template>
+<script>
+const columns = [
+  {
+    dataIndex: 'name',
+    key: 'name',
+    slots: { title: 'customTitle' },
+    scopedSlots: { customRender: 'name' },
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    scopedSlots: { customRender: 'tags' },
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    scopedSlots: { customRender: 'action' },
+  },
+];
+
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
+
+export default {
+  data() {
+    return {
+      data,
+      columns,
+    };
+  },
+};
+</script>
+-->
