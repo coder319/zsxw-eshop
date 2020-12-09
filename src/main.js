4e20867327127502f9c903f9bb3f053d9b26d7ed
coder-wtr <921506402@qq.com>
2020-12-09 23:32:23 +0800

    axios拦截、全局默认配置与请求方法封装的分离

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
-import http from './assets/js/axios'
+import http from './assets/js/http'
 
 Vue.prototype.$http = http
 
