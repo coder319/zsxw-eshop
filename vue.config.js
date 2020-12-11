a4846a9763c72fa839eaeb0c771fca30629ab48b
coder-wtr <921506402@qq.com>
2020-12-11 13:57:55 +0800

    添加路径别名

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
+const path = require('path');
+
+function resolve(dir) {
+    return path.join(__dirname, dir);
+}
 module.exports = {
     css: {
         loaderOptions: {
@@ -5,5 +10,13 @@ module.exports = {
                 javascriptEnabled: true
             }
         }
+    },
+    chainWebpack:(config)=>{
+        //set第一个参数：设置的别名，第二个参数：设置的路径
+        config.resolve.alias
+          .set('@',resolve('./src'))
+          .set('components',resolve('./src/assets/components'))
+          .set('js',resolve('./src/assets/js'))
+          .set('image',resolve('./src/assets/image'))
     }
 }
