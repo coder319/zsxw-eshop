7bcf611be4858e6c94a13da41fe7bb4f2433c4aa
coder-wtr <921506402@qq.com>
2020-12-11 15:24:35 +0800

    未处理翻页

--- src/views/Search/Search.vue
+++ src/views/Search/Search.vue
@@ -1,11 +1,29 @@
 <template>
-
+  <div>
+    <nav-bar></nav-bar>
+    <div class="cm_content">
+      <div class="cm_main">
+        <book></book>
+        <book></book>
+        <book></book>
+        <book></book>
+        <book></book>
+      </div>
+    </div>
+    <cm_footer></cm_footer>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: "Search"
-  }
+import navBar from "components/NavBar"
+import cm_footer from "components/Footer"
+import Book from 'components/Book2'
+export default {
+  name: "Search",
+  components:{
+    navBar,cm_footer,Book
+  },
+}
 </script>
 
 <style scoped>
