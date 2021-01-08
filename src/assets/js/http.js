773a9be52764de0116ab75084967f5e0e4dcb0e9
coder-wtr <921506402@qq.com>
2021-01-08 23:47:43 +0800

    登陆和获取验证码

--- src/assets/js/http.js
+++ src/assets/js/http.js
@@ -1,6 +1,25 @@
+/*
+ * @Description: 
+ * @Author: Wangtr
+ * @Date: 2020-12-09 23:29:09
+ * @LastEditTime: 2021-01-08 23:28:15
+ * @LastEditors: Wangtr
+ * @Reference: 
+ */
 import axios from './axios'
-axios.defaults.baseURL = 'http://127.0.0.1'
+axios.defaults.baseURL = 'http://118.31.229.85:9999'
 axios.defaults.headers = {}
 export default {
-
+    getVerificationCode() {
+        return axios({
+            url: '/api/verificationCode'
+        })
+    },
+    login(data) {
+        return axios({
+            url: '/api/login',
+            method: 'post',
+            data: data
+        })
+    }
 }
