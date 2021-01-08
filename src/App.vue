98283332d789532877077de1cd7e1c14ac423973
coder-wtr <921506402@qq.com>
2021-01-08 23:46:54 +0800

    全局loading

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,27 @@
+<!--
+ * @Description: 
+ * @Author: Wangtr
+ * @Date: 2020-11-14 09:37:58
+ * @LastEditTime: 2021-01-08 23:38:47
+ * @LastEditors: Wangtr
+ * @Reference: 
+-->
 <template>
   <div id="app">
-    <router-view />
+    <a-spin :spinning="this.$store.state.LOADING_NUM > 0" :delay="500" size="large" tip="loading...">
+      <router-view />
+    </a-spin>
   </div>
 </template>
+
+<script>
+import { Spin } from 'ant-design-vue'
+export default {
+  components: {
+    ASpin: Spin
+  }
+}
+</script>
 
 <style lang="less">
 @import "./assets/style/common.css";
--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,11 @@
+/*
+ * @Description: 
+ * @Author: Wangtr
+ * @Date: 2020-11-14 09:37:58
+ * @LastEditTime: 2020-12-31 13:35:36
+ * @LastEditors: Wangtr
+ * @Reference: 
+ */
 import Vue from 'vue'
 import Vuex from 'vuex'
 
@@ -5,8 +13,15 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    LOADING_NUM: 0
   },
   mutations: {
+    LOADING_START(state) {
+      this.state.LOADING_NUM++;
+    },
+    LOADING_END(state) {
+      this.state.LOADING_NUM--;
+    }
   },
   actions: {
   },
