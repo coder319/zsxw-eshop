2cebffe382b0aacd393f2de3a39310b8a97a963d
coder-wtr <921506402@qq.com>
2021-01-27 15:22:36 +0800

    添加全局modal组件

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
  * @Description: 入口js文件
  * @Author: Wangtr
  * @Date: 2020-11-14 09:37:58
- * @LastEditTime: 2021-01-22 19:00:38
+ * @LastEditTime: 2021-01-27 15:03:05
  * @LastEditors: Wangtr
  */
 import Vue from 'vue';
@@ -11,14 +11,18 @@ import router from './router';
 import store from './store';
 // import Antd from 'ant-design-vue'
 // import 'ant-design-vue/dist/antd.css'
-import { FormModel, Modal } from 'ant-design-vue';
+import { FormModel, Modal, Message } from 'ant-design-vue';
 import http from './assets/js/http';
 
 Vue.prototype.$http = http;
+Vue.prototype.$message = Message;
 
 Vue.config.productionTip = false;
 Vue.use(FormModel, Modal);
 // Vue.use(Antd)
+Vue.prototype.$error = Modal.error;
+Vue.prototype.$success = Modal.success;
+Vue.prototype.$warning = Modal.warning;
 
 new Vue({
   router,
