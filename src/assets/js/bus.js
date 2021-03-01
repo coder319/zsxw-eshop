7b9e860781d986d919328c513563834fb51c69e6
coder-wtr <921506402@qq.com>
2021-03-01 15:56:46 +0800

    事件总线

---
+++ src/assets/js/bus.js
@@ -0,0 +1,11 @@
+/*
+ * @Description: 事件总线js
+ * @Author: Wangtr
+ * @Date: 2021-03-01 15:47:35
+ * @LastEditTime: 2021-03-01 15:47:55
+ * @LastEditors: Wangtr
+ * @Reference:
+ */
+import Vue from 'vue';
+export default new Vue();
+
